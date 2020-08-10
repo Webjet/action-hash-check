8719b0a9971dc3834911cdf5aa2451bae6058f7b
vinh-ngo-webjet <vinh.ngo@webjet.com.au>
2020-08-10 22:34:47 +1000

    hash check

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const github = require('@actions/github');
 
 try {
   const commit = github.context.sha;
-  const latest = require('child_process').execSync('git rev-parse HEAD').toString();
-  console.log(latest);
+  const latest = require('child_process').execSync('git rev-parse HEAD').toString().trim();
+  if (commit != latest) {
+    core.setFailed(`Commit ${commit} is not latest`)
+  } else {
+    console.log(`Hash check passed`);
+  }
   
 } catch (error) {
   core.setFailed(error.message);
